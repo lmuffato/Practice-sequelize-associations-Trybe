7c6171f040b32a18979a9e534772419bfd749975
lmuffato <lmiremuffato@gmail.com>
2021-10-20 14:21:39 -0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
-const bodyParser = require('body-parser');
+/*
+npm init -y
+npm i express nodemon sequelize mysql2
+npm i sequelize-cli
+npx sequelize-cli init
+
+// Criar banco de dados com base na configuração do config.json
+npx sequelize db:create
+
+// Criar as migrations
+npx sequelize migration:generate --name create-plans
+npx sequelize migration:generate --name create-patients
+npx sequelize migration:generate --name create-surgeries
+npx sequelize migration:generate --name create-patient_surgeries
+
+// Executar as migrations
+npx sequelize db:migrate
+
+// Desfazer as migrations
+npx sequelize db:migrate:undo:all
+
+// Povoar banco de dados
+// Criar o seed
+npx sequelize seed:generate --name plans
+npx sequelize seed:generate --name patients
+npx sequelize seed:generate --name sugeries
+npx sequelize seed:generate --name patient_sugeries
+
+// Executar as seeds
+npx sequelize db:seed:all
+
+// Desfazer as seeds
+npx sequelize db:seed:undo:all
+*/
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const {
+  getAllPatientsPlans,
+  getAllPatientsSurgeries,
+  createPatients,
+  getPatientsAndSurgeriesNoDoctor,
+} = require('./controllers/patientsController');
+
+const getAllPlans = require('./controllers/plansController');
+const getDoctorSurgeries = require('./controllers/surgeriesController');
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = 3000;
+app.post('/', createPatients)
+app.get('/all', getAllPatientsPlans);
+app.get('/surgeries', getAllPatientsSurgeries);
+app.get('/surgeries/nodoctor', getPatientsAndSurgeriesNoDoctor);
+app.get('/surgeries/:name', getDoctorSurgeries);
+app.get('/:id', getAllPlans);
 
-app.listen(PORT, () => {
-  console.log(`Port: ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
